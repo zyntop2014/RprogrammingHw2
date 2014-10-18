becc92d52267d2ae32d50f1ac5fae6298b02b315
zyntop2014 <zyntop@gmail.com>
2014-10-18 11:22:41 -0600

    commit

---
+++ cachematrix.R
@@ -0,0 +1,46 @@
+## This pgromming is to write a pair of functions that cache the inverse of a matrix.
+##
+
+## This function creates a special "matrix" object that can cache its inverse. 
+
+makeCacheMatrix <- function(x = matrix()) {
+  
+  
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse<- function(solve) m <<- solve
+    getinverse<- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+
+  
+  
+
+}
+
+
+## This function computes the inverse of the special "matrix" returned 
+##by makeCacheMatrix above
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+ 
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("Caching the Inverse of a Matrix")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+  }
+  
+  
+  
+}
